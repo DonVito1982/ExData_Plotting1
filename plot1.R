6d9aa39178f3969fa7373a3d9b1635dc255c1757
Victor R Zambrano <donvito4571@gmail.com>
2015-12-12 21:23:37 -0430

    debug plot1 and add plots

--- plot1.R
+++ plot1.R
@@ -4,13 +4,16 @@ plot1 <- function(){
     library(grDevices)
     
     # First we need to extract ONLY the needed data
-    data <- fread("household_power_consumption.txt", na.strings = "?")[Date == "1/2/2007" | Date == "2/2/2007",
-                                                                       .(Global_active_power)]
+    dataFile <- "household_power_consumption.txt"
+    date1 <- "1/2/2007"
+    date2 <- "2/2/2007"
+    data <- fread(dataFile, na.strings = "?")[Date == date1 | Date == date2,
+                .(Global_active_power)]
     # Open the png device to create plot1.png
     png("plot1.png")
     
     # Plott the histogram
-    hist(intento4$Global_active_power, # the values to be plotted
+    hist(data$Global_active_power, # the values to be plotted
          main = "Global Active Power", # the plot title
          col = "red",                  # The red bins in the histogram are set this way
          xlab = "Global active power (Kilowatts)") # Label for "X" axis
